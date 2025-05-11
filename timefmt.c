c50adee189f011c6454b3768e3a9f5c5c8da4d50
Maxim <maxim@andromenet.org>
2025-05-11 23:40:24 +0200

    Fix time formatting
    Previously timefmt would print e.g. 2m121s instead of 2m1s.

--- timefmt.c
+++ timefmt.c
@@ -8,13 +8,13 @@ char *timefmt(long seconds) {
   if (seconds < 60) { // Minute
     sprintf(ret, "%lds", seconds);
   } else if (seconds < 3600) { // Hour
-    sprintf(ret, "%ldm%lds", seconds / 60, seconds);
+    sprintf(ret, "%ldm%lds", seconds / 60, seconds % 60);
   } else if (seconds < 86400) { // Day
-    sprintf(ret, "%ldh%ldm", seconds / 3600, seconds / 60);
+    sprintf(ret, "%ldh%ldm", seconds / 3600, (seconds / 60) % 3600);
   } else if (seconds < 604800) { // Week
-    sprintf(ret, "%ldd%ldh", seconds / 86400, seconds / 3600);
+    sprintf(ret, "%ldd%ldh", seconds / 86400, (seconds / 3600) % 86400);
   } else {
-    sprintf(ret, "%ldw%ldd", seconds / 604800, seconds / 86400);
+    sprintf(ret, "%ldw%ldd", seconds / 604800, (seconds / 86400) % 604800);
   }
   return ret;
 }
